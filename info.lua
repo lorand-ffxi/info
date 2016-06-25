93699dc581eff70b9075a7df141f6ffe26b561db
lorand-ffxi <lorand.ffxi@gmail.com>
2016-06-25 14:22:25 -0400

    moved info_share.lua into libs/lor/lor_exec.lua

--- info.lua
+++ info.lua
@@ -1,109 +1,109 @@
 _addon.name = 'info'
 _addon.author = 'Lorand'
 _addon.command = 'info'
-_addon.version = '1.4.5'
-_addon.lastUpdate = '2015.04.02'
+_addon.version = '1.5.0'
+_addon.lastUpdate = '2016.06.25'
 
 --[[
-	Info is a Windower addon for FFXI that is designed to allow users to view
-	data that is available to Windower from within the game.
+    Info is a Windower addon for FFXI that is designed to allow users to view
+    data that is available to Windower from within the game.
 --]]
 
 require('luau')
-res = require('resources')
-packets = require('packets')
-slips = require('slips')
-extdata = require('extdata')
-config = require('config')
+require('lor/lor_utils')
+_libs.lor.req('functional', 'tables', 'strings', 'chat', 'exec')
 
-require 'info_share'
+local res = require('resources')
+local packets = require('packets')
+local slips = require('slips')
+local extdata = require('extdata')
+local config = require('config')
 
 local showKB = false
 local showAnActionPacket = false
+local exec_funcs = nil
 
 windower.register_event('addon command', function (command,...)
-	command = command or 'help'
-	local args = {...}
-	
-	if command:lower() == 'reload' then
-		windower.send_command('lua reload '.._addon.name)
-	elseif command:lower() == 'unload' then
-		windower.send_command('lua unload '.._addon.name)
-	elseif command:lower() == 'showkb' then
-		showKB = not showKB
-	elseif command:lower() == 'actionpacket' then
-		showAnActionPacket = true
-	elseif command:lower() == 'geardump' then
-		geardump()
-	elseif command:lower() == 'item_info' then
-		item_info()
-	elseif command:lower() == 'ws_properties' then
-		ws_properties()
-	elseif command:lower() == 'ortest' then
-		orTest()
-	else
-		info.process_input(command, args)
-	end
+    local cmd = command:lower() or 'help'
+    
+    if exec_funcs[cmd] ~= nil then
+        exec_funcs[cmd]()
+    elseif cmd == 'reload' then
+        windower.send_command('lua reload '.._addon.name)
+    elseif cmd == 'unload' then
+        windower.send_command('lua unload '.._addon.name)
+    elseif cmd == 'showkb' then
+        showKB = not showKB
+    elseif cmd == 'actionpacket' then
+        showAnActionPacket = true
+    else
+        _libs.lor.exec.process_input(command, {...})
+    end
 end)
+
 
 windower.register_event('incoming chunk', function(id,data)
-	if showAnActionPacket then
-		if id == 0x28 then
-			local parsed = packets.parse('incoming', data)
-			print_table(parsed, 'Action Packet (0x028)')
-			if parsed['Target 1 Action 1 Message'] == 230 then
-				showAnActionPacket = false
-			end
-		end
-	end
+    if showAnActionPacket then
+        if id == 0x28 then
+            local parsed = packets.parse('incoming', data)
+            print_table(parsed, 'Action Packet (0x028)')
+            if parsed['Target 1 Action 1 Message'] == 230 then
+                showAnActionPacket = false
+            end
+        end
+    end
 end)
+
 
 windower.register_event('keyboard', function (dik, flags, blocked)
-	if showKB then
-		atc('[Keyboard] dik: '..tostring(dik)..', flags: '..tostring(flags)..', blocked: '..tostring(blocked))
-	end
+    if showKB then
+        atc('[Keyboard] dik: '..tostring(dik)..', flags: '..tostring(flags)..', blocked: '..tostring(blocked))
+    end
 end)
 
-function atc(c, msg)
-	if (type(c) == 'string') and (msg == nil) then
-		msg = c
-		c = 0
-	end
-	windower.add_to_chat(c, windower.to_shift_jis(msg))
-end
 
 function ws_properties()
-	atc(166,'Weaponskill properties')
-	local wses = res.weapon_skills
-	local skills = res.skills
-	for _,ws in pairs(res.weapon_skills) do
-		local name = ws.en
-		local skill = (ws.skill ~= nil) and skills[ws.skill].en or ''
-		local a,b,c = tostring(ws.skillchain_a),tostring(ws.skillchain_b),tostring(ws.skillchain_c)
-		atc(name..','..skill..','..a..','..b..','..c)
-	end
+    atc(166,'Weaponskill properties')
+    local wses = res.weapon_skills
+    local skills = res.skills
+    for _,ws in pairs(res.weapon_skills) do
+        local name = ws.en
+        local skill = (ws.skill ~= nil) and skills[ws.skill].en or ''
+        local a,b,c = tostring(ws.skillchain_a),tostring(ws.skillchain_b),tostring(ws.skillchain_c)
+        atc(name..','..skill..','..a..','..b..','..c)
+    end
 end
+
 
 function orTest()
-	local a = 'original'
-	local b = b or 'replacement'
-	local c = false
-	local d = c or b
-	atc('OR Test result: '..b)
+    local a = 'original'
+    local b = b or 'replacement'
+    local c = false
+    local d = c or b
+    atc('OR Test result: '..b)
 end
 
+
 function item_info()
-	local inv = windower.ffxi.get_items().inventory
-	for slot,itbl in pairs(inv) do
-		local irt = res.items[itbl.id]
-		if (irt == nil) then
-			atc('['..slot..'] '..itbl.id..' | NO INFO')
-		else
-			local augs = get_augment_string(itbl)
-			atc('['..slot..'] '..itbl.id..' | '..irt.enl:capitalize()..' | '..tostring(augs))
-		end
-	end
+    local inv = windower.ffxi.get_items().inventory
+    for slot,itbl in pairs(inv) do
+        if type(slot) == 'number' then
+            local irt, iid = nil, nil
+            if not any_eq(type(itbl), 'number', 'boolean') then
+                iid = itbl.id
+                irt = res.items[iid]
+            end
+            
+            if (irt == nil) then
+                atc('[%2s] %s | NO INFO':format(slot, tostring(iid)))
+            else
+                local augs = get_augment_string(itbl)
+                atc('[%2d] %s | %s | %s':format(slot, iid, irt.enl:capitalize(), tostring(augs)))
+            end
+        end
+    end
 end
+
 
 function get_augment_string(item)
     local augments
@@ -131,38 +131,47 @@ function get_augment_string(item)
     end
 end
 
+
 function geardump()
-	local items = windower.ffxi.get_items()
-	local bags = {items.wardrobe,items.locker,items.storage,items.sack,items.satchel,items.inventory,items.safe,items.case}
-	
-	local gear = {}
-	for _,tbl in pairs(bags) do
-		for i = 1, 80 do
-			local itbl = tbl[i]
-			local irt = res.items[itbl.id]
-			if (irt ~= nil) then
-				local augstr = get_augment_string(itbl)
-				local iname = irt.enl:capitalize()
-				gear['id'..itbl.id] = {id=itbl.id,name=iname,augs=augstr}
-			end
-		end
-	end
-	for slipid,sitems in pairs(slips.get_player_items()) do
-		for idx,iid in pairs(sitems) do
-			if (idx ~= 'n') then
-				local irt = res.items[iid]
-				if (irt ~= nil) then
-					local iname = irt.enl:capitalize()
-					gear['id'..iid] = {id=iid,name=iname}
-				else
-					atc(123,'Error: Unknown item with id '..tostring(iid))
-				end
-			end	
-		end
-	end
-	config.load(gear)
-	atc('Dumped gear.')
+    local items = windower.ffxi.get_items()
+    local bags = {items.wardrobe,items.wardrobe2,items.locker,items.storage,items.sack,items.satchel,items.inventory,items.safe,items.case}
+    
+    local gear = {}
+    for _,tbl in pairs(bags) do
+        for i = 1, 80 do
+            local itbl = tbl[i]
+            local irt = res.items[itbl.id]
+            if (irt ~= nil) then
+                local augstr = get_augment_string(itbl)
+                local iname = irt.enl:capitalize()
+                gear['id'..itbl.id] = {id=itbl.id,name=iname,augs=augstr}
+            end
+        end
+    end
+    for slipid,sitems in pairs(slips.get_player_items()) do
+        for idx,iid in pairs(sitems) do
+            if (idx ~= 'n') then
+                local irt = res.items[iid]
+                if (irt ~= nil) then
+                    local iname = irt.enl:capitalize()
+                    gear['id'..iid] = {id=iid,name=iname}
+                else
+                    atc(123,'Error: Unknown item with id '..tostring(iid))
+                end
+            end 
+        end
+    end
+    config.load(gear)
+    atc('Dumped gear.')
 end
+
+
+exec_funcs = {
+    ['geardump'] = geardump,
+    ['item_info'] = item_info,
+    ['ws_properties'] = ws_properties,
+    ['ortest'] = orTest
+}
 
 -----------------------------------------------------------------------------------------------------------
 --[[
